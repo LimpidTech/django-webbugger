34c0175a30e136556de89ddd961f1edb69962125
Brandon R. Stoner <monokrome@monokro.me>
2011-06-11 05:47:22 +0000

    Removed an unncessary comment.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='django-webbugger',
       description='Basic webbugger/tracking beacon for django',
       author='Brandon R. Stoner',
       author_email='monokrome@monokro.me',
-      url='http://monokro.me/',  # This needs updated once my site is active.
+      url='http://monokro.me/',
       packages=['webbugger'],
 )
 
